9485c008e73f0cac53fe3af2570ab3830bc1bfff
farutnen <farutnen@gmail.com>
2025-05-12 02:53:48 +0000

    hosting firebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <Link to="/" className="flex items-center">
-            <img src="/toyota-logo.png" alt="Toyota Kansai Logo" className="h-10 w-auto mr-3" style={{ maxHeight: '40px', width: 'auto' }} />
+            <img src="/toyota-logo.png" alt="Toyota Kansai Logo" className="h-6 w-auto mr-2" style={{ maxHeight: '40px', width: 'auto' }} />
             <span className="text-2xl font-bold text-toyota-black tracking-wide">TOYOTA</span>
           </Link>
           
